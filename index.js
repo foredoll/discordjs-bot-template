d1840e93bc530c71a6cc7ab407316e051fea6499
foredoll <80313453+foredoll@users.noreply.github.com>
2021-04-27 10:08:35 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,82 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const chalk = require("chalk");
+const moment = require("moment");
+const config = require("./config.json");
+
+client.login(process.env.TOKEN);
+
+client.on('ready', () => {
+  client.user.setGame("in my swamp");
+  client.user.setStatus('online');										    
+});
+
+const log = message => {
+console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+}; 
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+fs.readdir('./commands/', (err, files) => {
+  if (err) console.error(err); 
+  log(`Loading a total of ${files.length} commands.`);
+  files.forEach(f => {
+    let props = require(`./commands/${f}`); 
+    log(`Loading Command: ${props.help.name}..`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.modules = new Discord.Collection();
+fs.readdir('./modules/', (err, modules) => {
+  if (err) console.error(err); 
+  log(`Loading a total of ${modules.length} modules.`);
+  modules.forEach(m => {
+    let loadData = require(`./modules/${m}`); 
+    log(`Loading Module: ${loadData.moduledata.name}..`);
+    client.modules.set(loadData.moduledata.name, loadData);
+  });
+});
+
+fs.readdir('./events/', (err, events) => {
+  if (err) console.error(err); 
+  log(`Loading a total of ${events.length} events.`);
+  events.forEach(e => {
+    let loadData = require(`./events/${e}`);
+  });
+});
+
+client.reload = command => { 
+  return new Promise((resolve, reject) => {
+    try { 
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      let cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  let permlvl = 0;
+  if (message.author.id === config.botOwner) permlvl = 4;
+  return permlvl;
+};
+
+process.on("unhandledRejection", err => {
+console.log(err.stack);
+});
