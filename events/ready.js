86aad84a9675dd9981afe07f8360df8919d41476
foredoll <80313453+foredoll@users.noreply.github.com>
2021-04-27 10:40:46 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,8 @@ module.exports = {
 	once: true,
    execute(client) {
        console.log(`Ready! Logged in as ${client.user.tag}`);
+	   client.user.setActivity("YouTube", { type: 'WATCHING' })
+	   client.user.setStatus('dnd')
+	   })
 	},
 };
