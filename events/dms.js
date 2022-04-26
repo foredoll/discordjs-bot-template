a793191bfecaa5c7b632e87b045c38dc559a0334
foredoll <80313453+foredoll@users.noreply.github.com>
2022-04-25 22:08:44 -0700

    Delete events directory

--- events/dms.js
+++
@@ -1,6 +0,0 @@
-const {  logChannelID } = require('../config.json')
-
-module.exports = (client, message) => {
-  if(message.channel.type != "dm") return;
-  client.channels.cache.get(logChannelID).send(`Message sent by ${message.author.tag} (${message.author.id}): ${message.content}. Time: ${message.createdAt}`)
-}
--- events/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'ready',
-	once: true,
-   execute(client) {
-       console.log(`Ready! Logged in as ${client.user.tag}`);
-	   client.user.setActivity("YouTube", { type: 'WATCHING' })
-	   client.user.setStatus('dnd')
-	   }
-};
