ece28ae4cfe71b1189b308e4808af37caf9904a2
foredoll <80313453+foredoll@users.noreply.github.com>
2022-04-25 22:11:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,18 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const prefix = ".";
+const fs = require('node:fs');
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-client.cooldowns = new Discord.Collection();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args, client));
+		client.once(event.name, (...args) => event.execute(...args));
 	} else {
-		client.on(event.name, (...args) => event.execute(...args, client));
+		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
 
-const commandFolders = fs.readdirSync('./commands');
-
-for (const folder of commandFolders) {
-	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const command = require(`./commands/${folder}/${file}`);
-		client.commands.set(command.name, command);
-	}
-}
-
-client.login(process.env.TOKEN);
+client.login(token);
